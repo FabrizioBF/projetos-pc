b0756daf5b4c4c14aab661c107de938778686786
Fabrízio Farias <fabrizio.b.f@gmail.com>
2024-02-22 23:18:07 -0300

    exercicio2 adicionado

---
+++ exercicio2.py
@@ -0,0 +1,10 @@
+
+# Solicita ao usuário para digitar os dois números
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+# Calcula a média aritmética dos números
+media = (numero1 + numero2) / 2
+
+# Exibe o resultado da média aritmética
+print("A média aritmética dos números é:", media)
